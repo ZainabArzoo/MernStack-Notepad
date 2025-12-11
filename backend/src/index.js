01f3a7984eb7c08ea62e6e596576699a9716f6ff
Zainab Arzoo <zainabarzoo.14@gamil.com>
2025-12-11 20:40:08 +0500

    updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,8 +3,7 @@ import notesRoute from "./routes/notesRoutes.js"
 import { connectDB } from "../config/db.js";
 import cors from "cors"
 import dotenv from "dotenv"
-
-
+import path from "path"
 
 
 
@@ -12,15 +11,16 @@ dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve();
 
 
-
-
-
-
-app.use(cors({
-    origin:"http://localhost:5173"
-}))
+if (process.env.NODE_ENV !== "production") {
+    app.use(
+        cors({
+            origin: "http://localhost:5173",
+        })
+    );
+}
 
 app.use(express.json())
 
@@ -28,7 +28,13 @@ app.use(express.json())
 app.use("/api/notes", notesRoute)
 
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 
 
